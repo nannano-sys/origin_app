5dc49b852241a14fb026410b51eb6d6d30f97e47
nannano-sys <nannano1868@gmeil.com>
2020-11-20 18:05:18 +0900

    マイグレーションファイル編集

--- db/migrate/20201120085122_create_tweets.rb
+++ db/migrate/20201120085122_create_tweets.rb
@@ -1,7 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.text :content, null: false #ActiveRecord実装まで
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
