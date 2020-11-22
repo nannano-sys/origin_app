8b667a04741a79237ac241e3b87a03d9903bffa3
nannano-sys <nannano1868@gmeil.com>
2020-11-22 13:48:22 +0900

    コントローラ編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to tweet_path(@comment.tweet)
+    else
+      @tweet = @comment.tweet
+      @comment = @tweet.comments
+      render :show
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
+
+
