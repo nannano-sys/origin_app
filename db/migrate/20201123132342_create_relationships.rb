bb0d7e8d72af9620c8d4e192353980317a632e30
nannano-sys <nannano1868@gmeil.com>
2020-11-24 12:16:50 +0900

    DB編集

--- db/migrate/20201123132342_create_relationships.rb
+++ db/migrate/20201123132342_create_relationships.rb
@@ -1,11 +1,11 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.references :follower, foreign_key: {to_table: :users}#followerテーブルはないので、usersテーブルを参照するよう記述
-      t.references :following, foreign_key: {to_table: :users} #followingテーブルはないので、usersテーブルを参照するよう記述
+      t.references :user, foreign_key: true
+      t.references :follow, foreign_key: {to_table: :users} #followingテーブルはないので、usersテーブルを参照するよう記述
       t.timestamps
 
-      t.index [:follower_id, :following_id], unique: true #user(フォローする側)とfollow(フォローされる側)の組み合わせが重複しないようにする
+      t.index [:user_id, :follow_id], unique: true #user(フォローする側)とfollow(フォローされる側)の組み合わせが重複しないようにする
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,13 @@ ActiveRecord::Schema.define(version: 2020_11_23_132342) do
   end
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "follower_id"
-    t.bigint "following_id"
+    t.bigint "user_id"
+    t.bigint "follow_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
-    t.index ["following_id"], name: "index_relationships_on_following_id"
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_132342) do
 
   add_foreign_key "comments", "tweets"
   add_foreign_key "comments", "users"
-  add_foreign_key "relationships", "users", column: "follower_id"
-  add_foreign_key "relationships", "users", column: "following_id"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
   add_foreign_key "tweets", "users"
 end
