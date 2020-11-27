d2cffdf2d84c0fc5d71b5781386ffce34fce3313
nannano-sys <nannano1868@gmeil.com>
2020-11-27 18:58:04 +0900

    Revert "tweet_tagモデル作成"
    This reverts commit e7c0289ca050eb26b2cbb9a39dee1c4bc81022f7.

--- app/models/tweet_tag.rb
+++
@@ -1,2 +0,0 @@
-class TweetTag < ApplicationRecord
-end
--- db/migrate/20201127065859_create_tweet_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreateTweetTags < ActiveRecord::Migration[6.0]
-  def change
-    create_table :tweet_tags do |t|
-
-      t.timestamps
-    end
-  end
-end
