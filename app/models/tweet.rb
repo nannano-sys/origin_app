2b6f707f4f5fdb184810544b8a78a02a9c71a241
nannano-sys <nannano1868@gmeil.com>
2020-12-18 21:24:48 +0900

    エラー修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,11 +1,11 @@
 class Tweet < ApplicationRecord
 
-  with_options presence: true, unless: :was_attached? do
+  with_options presence: true do
   validates :title, presence:{message: "が空欄では投稿できません"}
   validates :content, presence:{message: "が空欄では投稿できません"},length: {maximum:500, message: "ander 1000!"}
   end
 
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many_attached :image #複数のファイルを各レコードと紐付ける記述
+  # has_many_attached :image 複数のファイルを各レコードと紐付ける記述
 end
